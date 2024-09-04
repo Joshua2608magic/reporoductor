cdd94bf66b15956c9eb87f8679fe2d4a24af9014
Josue <josuelivingson@gmail.com>
2024-09-04 12:07:09 -0400

    reproduvtor2

--- scipt.js
+++ scipt.js
@@ -1,10 +1,31 @@
 
 
 axios.get('https://leonardoapi.onrender.com/songs')
-  .then(function (response) {
-    // manejar respuesta exitosa
-    console.log(response);
-  })
+    .then(function (response) {
+        // manejar respuesta exitosa
+        console.log(response.data.songs);
+
+        response.data.map((song) => {
+            const container = document.getElementById('track-list')
+
+            const div = document.createElement('div')
+            div.setAttribute('class', "musica-1")
+
+            div.innerHTML = `
+                <img src="assets/Img.jpg" alt="">
+
+                <div class="music">
+                    <h3>Vivir la vida</h3>
+                    <p>Marck Anthony </p>
+
+                </div>
+`
+
+div.appendChild(div)
+
+        })
+
+    })
 
 
 // console.log(datoString)
