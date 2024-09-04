2748d4282c79d399618f3740c872e802a49aa309
Josue <josuelivingson@gmail.com>
2024-09-04 11:25:44 -0400

    hice el reproduvtor

---
+++ scipt.js
@@ -0,0 +1,12 @@
+
+
+axios.get('https://leonardoapi.onrender.com/songs')
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response);
+  })
+
+
+// console.log(datoString)
+// const datoString ="Josue Livingston"
+// const datoNumber =7
